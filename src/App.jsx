e4539ff976b0ca430882bd6a09534064f63d45b1
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-04-25 14:37:18 +0200

    added image assets,pages and configured react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
+import React,{useState, useEffect, createContext, useContext} from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Stories from './pages/Stories';
+import Features from './pages/Features';
+import Pricing from './pages/Pricing';
 
+const AppContext = createContext()
 function App() {
+  const [size, setSize] = useState(window.innerWidth);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return (
-    <>
+    useEffect(() => {
+      const handleResize = () => {
+        setSize(window.innerWidth);
+        if (window.innerWidth >= 640) {
+          setIsMenuOpen(false);
+        }
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+      document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto';
+      return () => {
+        document.body.style.overflow = 'auto';
+      };
+  }, [isMenuOpen]);
  
-    </>
+  return (
+    <AppContext.Provider value={{size, setIsMenuOpen, isMenuOpen}}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Stories/>}/>
+        <Route path='*' element={<Stories/>}/>
+        <Route path='/Features' element={<Features/>}/>
+        <Route path='/Pricing' element={<Pricing/>}/>
+      </Routes>
+      <Footer />
+    </AppContext.Provider>
   )
 }
 
-export default App
+export const useAppContext = () => {
+  return useContext(AppContext)
+};
+
+export default App;
